1cdb92c5acb20108ccdb7a4c2050ce4086b5b3db
kevin5112 <kevin5112@yahoo.com>
2025-08-27 18:58:33 -0700

    remove require

--- src/app/api/generate-ics/route.ts
+++ src/app/api/generate-ics/route.ts
@@ -4,8 +4,6 @@ import { getIcsDatePartsLocal } from "@/utils/geticsdate";
 import { ScheduleEntry } from "@/utils/parser";
 
 export async function POST(req: NextRequest) {
-  console.log("ICS version:", require("ics/package.json").version);
-
   const data = await req.json();
 
   const events = data.map((entry: ScheduleEntry) => {
